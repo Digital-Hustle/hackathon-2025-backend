fe274650767e8e8449e486e19892304c52ebc0ef
Danil <dabcom2013@gmail.com>
2025-10-26 10:03:20 +0300

    auth-ms. Add CORS config

--- services/auth-ms/src/main/java/ru/ci_trainee/authms/config/SecurityConfig.java
+++ services/auth-ms/src/main/java/ru/ci_trainee/authms/config/SecurityConfig.java
@@ -16,8 +16,13 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import ru.ci_trainee.authms.security.jwt.JwtTokenFilter;
 import ru.ci_trainee.authms.security.jwt.JwtTokenProvider;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -41,28 +46,42 @@ public class SecurityConfig {
     }
 
     @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://192.168.0.101:3000", "http://192.168.180.250:3000", "null", "http://localhost:8081", "https://websocketking.com"));  // Разрешить доступ с вашего фронтенда
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .anonymous(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
-                .exceptionHandling(configurer -> configurer
-                        .authenticationEntryPoint(
-                                (request, response, exception) -> {
-                                    response.setStatus(HttpStatus.UNAUTHORIZED.value());
-                                    response.getWriter().write("Unauthorized.");
-                                }
-                        )
-                        .accessDeniedHandler(
-                                (request, response, exception) -> {
-                                    response.setStatus(HttpStatus.FORBIDDEN.value());
-                                    response.getWriter().write("Forbidden.");
-                                }
-                        )
-                )
+//                .exceptionHandling(configurer -> configurer
+//                        .authenticationEntryPoint(
+//                                (request, response, exception) -> {
+//                                    response.setStatus(HttpStatus.UNAUTHORIZED.value());
+//                                    response.getWriter().write("Unauthorized.");
+//                                }
+//                        )
+//                        .accessDeniedHandler(
+//                                (request, response, exception) -> {
+//                                    response.setStatus(HttpStatus.FORBIDDEN.value());
+//                                    response.getWriter().write("Forbidden.");
+//                                }
+//                        )
+//                )
                 .authorizeHttpRequests(configurer -> configurer
                         .requestMatchers("/swagger-ui/**").permitAll()
                         .requestMatchers("/v3/api-docs/**").permitAll()
